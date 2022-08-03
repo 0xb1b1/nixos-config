897e408d926a35fd3135e0a0c0dece4a21b52a3f
0xb1b1 <voxelflee@protonmail.com>
2022-08-03 12:38:06 +0300

    Add .well-known directory to service.nginx seizure.icu FQDN

--- system/machine/shadow-nix/default.nix
+++ system/machine/shadow-nix/default.nix
@@ -105,6 +105,25 @@
     '';
 
     virtualHosts = {
+      "seizure.icu" = {
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 443;
+            ssl = true;
+          }
+          {
+            addr = "0.0.0.0";
+            port = 80;
+          }
+        ];
+        forceSSL = true;
+        sslCertificate = "/var/nginx/certs/seizure.icu/fullchain.pem";
+        sslCertificateKey = "/var/nginx/certs/seizure.icu/privkey.pem";
+        #locations."/".proxyPass = "http://127.0.0.1:23417";
+        root = "/var/http/seizure.icu";
+	locations."/.well-known/" = {};
+      };
       "jellyfin.seizure.icu" = {
         listen = [
 	  {

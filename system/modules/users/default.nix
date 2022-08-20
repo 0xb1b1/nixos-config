d03ce72d1c93c8e85780e449b39c95d218fa989d
0xb1b1 <voxelflee@protonmail.com>
2022-08-20 17:12:56 +0300

    Add OpenSSH pubkeys to user voxel

--- system/modules/users/default.nix
+++ system/modules/users/default.nix
@@ -12,6 +12,10 @@
       extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "adbusers" ];
       shell = pkgs.zsh;
       hashedPassword = "$6$cQWJCPMwSqsbh9r$xhnVcK.SONgK7P60uNPaJjoAtXXZdKbuy7YpKGzabWJuOte8LVNjNk4lTHEwvtX5SKagTgr24qwFxNkc3HRWY0";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZk4eNkp+63OWjGvEXz6xUwVU3rb7669IlLJYM54Dk0 voxel@outline"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUV9yrNWGZcxz0crQ7jIoLCAKCBiCkcrWzrxlLidky8 voxel@voyager"
+      ];
     };
     arina = {
       isNormalUser = true;

6b515b0560d3b000027020aac3ccc5954c6778c2
0xb1b1 <voxelflee@protonmail.com>
2022-08-24 17:26:10 +0300

    Set user UIDs and set global default shell to zsh

--- system/modules/users/default.nix
+++ system/modules/users/default.nix
@@ -4,13 +4,17 @@
   # User management settings.
   users.mutableUsers = false;
 
+  # Set default shell.
+  users.defaultUserShell = pkgs.zsh;
+
   # Define user accounts.
   users.users = {
     voxel = {
       isNormalUser = true;
       description = "voxel";
+      uid = 1000;
       extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "adbusers" ];
-      shell = pkgs.zsh;
+      useDefaultShell = true;
       hashedPassword = "$6$cQWJCPMwSqsbh9r$xhnVcK.SONgK7P60uNPaJjoAtXXZdKbuy7YpKGzabWJuOte8LVNjNk4lTHEwvtX5SKagTgr24qwFxNkc3HRWY0";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZk4eNkp+63OWjGvEXz6xUwVU3rb7669IlLJYM54Dk0 voxel@outline"
@@ -20,7 +24,8 @@
     arina = {
       isNormalUser = true;
       description = "arina";
-      shell = pkgs.zsh;
+      uid = 1001;
+      useDefaultShell = true;
       hashedPassword = "$6$cQWJCPMwSqsbh9r$mIALQWkszxd8W4lHHq9JLkJIOLzRnfre9RVBFoidi1zssk9rvPZEAvtRCMqc5tTkq95ZfOLIcLomAzbXkvX7F1";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINIbLUONM0K9idIRgTANd+soJRpeJgPhfC+fTJUCM7Bv arina@hachpachmak"

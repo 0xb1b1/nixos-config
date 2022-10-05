0b9397a22de1dc7330f2a68bf392552face2f401
0xb1b1 <voxelflee@protonmail.com>
2022-10-05 21:31:35 +0300

    Add datacats.seizure.icu to shadow-nix Nginx configuration
    Add datacats.seizure.icu to shadow-nix Nginx reverse proxy configuration

--- system/hosts/shadow-nix/modules/services/nginx/domains/seizure.icu/reverse.nix
+++ system/hosts/shadow-nix/modules/services/nginx/domains/seizure.icu/reverse.nix
@@ -255,5 +255,22 @@
       sslCertificateKey = "/var/nginx/certs/seizure.icu/privkey.pem";
       locations."/".proxyPass = "http://127.0.0.1:23550";
     };
+    "datacats.seizure.icu" = {
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 443;
+          ssl = true;
+        }
+        {
+          addr = "0.0.0.0";
+          port = 80;
+        }
+      ];
+      forceSSL = true;
+      sslCertificate = "/var/nginx/certs/seizure.icu/fullchain.pem";
+      sslCertificateKey = "/var/nginx/certs/seizure.icu/privkey.pem";
+      locations."/".proxyPass = "http://127.0.0.1:23432";
+    };
   };
 }

03e3dfc8d405c7a53d9ee0a66df3995fea4ed327
0xb1b1 <voxelflee@protonmail.com>
2022-10-29 02:19:54 +0300

    Add english.seizure.icu to shadow-nix nginx config
    Add english.seizure.icu to shadow-nix nginx reverse proxy configuration

--- system/hosts/shadow-nix/modules/services/nginx/domains/seizure.icu/reverse.nix
+++ system/hosts/shadow-nix/modules/services/nginx/domains/seizure.icu/reverse.nix
@@ -333,5 +333,25 @@
         proxyWebsockets = true;
       };
     };
+    "english.seizure.icu" = {  # English
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 443;
+          ssl = true;
+        }
+        {
+          addr = "0.0.0.0";
+          port = 80;
+        }
+      ];
+      forceSSL = true;
+      sslCertificate = "/var/nginx/certs/seizure.icu/fullchain.pem";
+      sslCertificateKey = "/var/nginx/certs/seizure.icu/privkey.pem";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:23501";
+        proxyWebsockets = true;
+      };
+    };
   };
 }

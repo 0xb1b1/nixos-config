91da3378a1a781c6d8a83c5188ab88f869dd7154
0xb1b1 <voxelflee@protonmail.com>
2022-11-22 03:44:15 +0300

    Add httpshare.seizure.icu to shadow nginx config
    Add httpshare.seizure.icu (Nginx share) to shadow nginx reverse proxy configuration

--- system/hosts/shadow-nix/modules/services/nginx/domains/seizure.icu/reverse.nix
+++ system/hosts/shadow-nix/modules/services/nginx/domains/seizure.icu/reverse.nix
@@ -393,5 +393,25 @@
         proxyWebsockets = true;
       };
     };
+    "httpshare.seizure.icu" = {
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 443;
+          ssl = true;
+        }
+        {
+          addr = "0.0.0.0";
+          port = 80;
+        }
+      ];
+      forceSSL = true;
+      sslCertificate = "/var/nginx/certs/seizure.icu/fullchain.pem";
+      sslCertificateKey = "/var/nginx/certs/seizure.icu/privkey.pem";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:23438";
+        proxyWebsockets = true;
+      };
+    };
   };
 }

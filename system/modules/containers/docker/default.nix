6f41e62b34c40795652b9c7a4a5b15b5b02bd9e2
0xb1b1 <voxelflee@protonmail.com>
2023-02-04 21:30:20 +0300

    [TEMP] Comment out Docker daemon settings
    - Refactor Docker daemon settings (use Nix language)
    - Comment out Docker daemon settings (Docker errors out)

--- system/modules/containers/docker/default.nix
+++ system/modules/containers/docker/default.nix
@@ -3,17 +3,17 @@
   virtualisation.docker = {
     enable = true;
     liveRestore = false;  # Not compatible with Docker Swarm
-    daemon.settings = {
-      "default-address-pools": [
-        {
-          "base": "172.20.0.0/16",
-          "size":24
-        },
-        {
-          "base": "172.21.0.0/16",
-          "size":24
-        }
-      ];
-    }
+    # daemon.settings = {
+    #   "default-address-pools" = [
+    #     {
+    #       "base" = "172.20.0.0/16";
+    #       "size" = 24;
+    #     }
+    #     {
+    #       "base" = "172.21.0.0/16";
+    #       "size" = 24;
+    #     }
+    #   ];
+    # };
   };
 }

2ebbc35a0bde2afa70a546719e473da2e90050b5
0xb1b1 <voxelflee@protonmail.com>
2023-02-20 18:33:55 +0300

    Remove user arzym on shadow-nix

--- system/hosts/shadow-nix/modules/users/default.nix
+++ system/hosts/shadow-nix/modules/users/default.nix
@@ -9,17 +9,5 @@
   # users.defaultUserShell = pkgs.zsh;  # Set in global configuration
 
   # Define user accounts.
-  users.users = {
-    arzym = {
-      isNormalUser = true;
-      description = "arzym";
-      uid = 1100;
-      useDefaultShell = true;
-      hashedPassword = "$6$pZQZXnA2AW0Y$7eWmfxPIuc7RfPcU8IpnKy32Yhz/g2lTRotIVzO7Sd3QuKyKkdTGZ15GD029G8xoP0SuQCkpsVRcGBoeHkkVn.";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBz0IhG7jBM1G/iwSamDQZg7H0NRqSgKF8C1M1GHyrta artem@Artems-MacBook-Air"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAP77rLMTbgHUXmhmCHJ4+suudNyrzbu8z7xdSk/KXgA arzym@Workstation"
-      ];
-    };
-  };
+  # users.users = {};
 }
